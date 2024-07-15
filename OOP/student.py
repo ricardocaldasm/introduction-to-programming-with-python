013f57b113147965a65c42ddfd81ce9f14a57d3b
ricardocaldasm <79159591+ricardocaldasm@users.noreply.github.com>
2024-07-15 11:11:24 -0300

    student

--- OOP/student.py
+++ OOP/student.py
@@ -1,16 +1,34 @@
 class Student:
-    def __init__(self, name, house):  # method - Adicionando variáveis a objetos
+    def __init__(
+        self, name, house, patronus
+    ):  # method - Adicionando variáveis a objetos
         if not name:
             raise ValueError("Missing name.")
         if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid house.")
         self.name = name
         self.house = house
+        self.patronus = patronus
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+    def charm(self):
+        match self.patronus:
+            case "Stag":
+                return "Stag"
+            case "Otter":
+                return "Otter"
+            case "Dog":
+                return "Dog"
+            case _:
+                return "Wand"
 
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}.")
+    print("Expecto Patronum!")
+    # print(f"{student.name} from {student.house}.")
 
 
 def get_student():
@@ -21,7 +39,8 @@ def get_student():
     """
     name = input("Name: ")
     house = input("House: ")
-    return Student(name, house)  # name, house -> objects
+    patronus = input("Patronus: ")
+    return Student(name, house, patronus)  # name, house -> objects
 
 
 if __name__ == "__main__":
